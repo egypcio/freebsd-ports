e72259a6438f6391a01ea8de9853e606eedfc3ca
Vinícius Zavam <egypcio@googlemail.com>
2021-10-30 18:23:19 +0000

    www/tor-browser: patch based on www/firefox-esr, bug1640982

--- www/tor-browser/files/patch-config_makefiles_rust.mk
+++ www/tor-browser/files/patch-config_makefiles_rust.mk
@@ -1,16 +1,11 @@
---- config/makefiles/rust.mk.orig	2020-09-01 13:46:40 UTC
+--- config/makefiles/rust.mk.orig	2021-10-30 18:14:10 UTC
 +++ config/makefiles/rust.mk
-@@ -46,8 +46,12 @@ endif
- cargo_rustc_flags = $(CARGO_RUSTCFLAGS)
- ifndef DEVELOPER_OPTIONS
- ifndef MOZ_DEBUG_RUST
-+#
-+### https://bugzilla.mozilla.org/show_bug.cgi?id=1640982
-+#
- # Enable link-time optimization for release builds.
--cargo_rustc_flags += -C lto
-+# cargo_rustc_flags += -C lto
-+#
+@@ -61,7 +61,7 @@ ifndef MOZ_DEBUG_RUST
+ # Enable link-time optimization for release builds, but not when linking
+ # gkrust_gtest.
+ ifeq (,$(findstring gkrust_gtest,$(RUST_LIBRARY_FILE)))
+-cargo_rustc_flags += -Clto
++### cargo_rustc_flags += -Clto	# www/firefox-esr, bug1640982
  endif
- endif
- 
+ # Versions of rust >= 1.45 need -Cembed-bitcode=yes for all crates when
+ # using -Clto.
